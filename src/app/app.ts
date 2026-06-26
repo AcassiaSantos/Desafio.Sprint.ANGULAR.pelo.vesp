ec422752d48122e06ce8c4160940b7374403c3a6
AcassiaSantos <acassiamsantos85@gmail.com>
2026-06-26 16:33:03 -0300

    Alterando complementos e adicionando pagina de imagens

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Cabecalho } from './componentes/cabecalho/cabecalho';
+import { Rodape } from './componentes/rodape/rodape';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Cabecalho,
+    Rodape
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('DesafioSprintAngular');
 }
